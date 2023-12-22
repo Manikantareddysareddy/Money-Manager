6aa3eb69f6c409a65d11270515125c12a57b7861
Manikantareddysareddy <149862326+Manikantareddysareddy@users.noreply.github.com>
2023-12-22 20:13:45 +0530

    Update index.js

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,17 +1,35 @@
 const TransactionItem = props => {
-  const {transactionItem} = props
-  const {title, amount, type} = transactionItem
+  const {transactionItem, DeleteHistory} = props
+  const {id, title, amount, type} = transactionItem
+  let newType
+  if (type === 'INCOME') {
+    newType = 'Income'
+  } else {
+    newType = 'Expenses'
+  }
+
+  const DeleteTransaction = () => {
+    DeleteHistory(id)
+  }
+
   return (
     <li className="list-container">
       <div className="History-details1">
         <p className="para1">{title}</p>
-        <p className="para1">{amount}</p>
-        <p className="para1">{type}</p>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
-          alt="delete"
-          className="delete-image"
-        />
+        <p className="para1">Rs {amount}</p>
+        <p className="para1">{newType}</p>
+        <button
+          type="button"
+          onClick={DeleteTransaction}
+          className="delete-btn"
+          data-testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+            alt="delete"
+            className="delete-image"
+          />
+        </button>
       </div>
     </li>
   )
